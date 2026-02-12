4d117a976e35ff2afe63cc59ce11597245371bf5
balaraju <balarajumukalla3@gmail.com>
2026-02-12 22:41:26 +0530

    remove direct port number from server.js

--- server/server.js
+++ server/server.js
@@ -10,19 +10,15 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // allow frontend connection
+    origin: "*",
   },
 });
 
-// When a client connects
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // Listen for message from client
   socket.on("send_message", (message) => {
     console.log("Message received:", message);
-
-    // Broadcast to ALL connected clients
     io.emit("receive_message", message);
   });
 
@@ -31,6 +27,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
